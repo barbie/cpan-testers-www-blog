14645d59b670d8caf0f81556fe1729f361e3ddf5
Barbie <barbie@missbarbell.co.uk>
2013-01-06 14:06:52 +0000

    updated version and matadata

--- lib/CPAN/Testers/WWW/Blog.pm
+++ lib/CPAN/Testers/WWW/Blog.pm
@@ -3,7 +3,7 @@ package CPAN::Testers::WWW::Blog;
 use warnings;
 use strict;
 
-our $VERSION = '1.04';
+our $VERSION = '1.05';
 
 1;
 
@@ -52,7 +52,7 @@ Miss Barbell Productions, L<http://www.missbarbell.co.uk/>
 
 =head1 COPYRIGHT & LICENSE
 
-  Copyright (C) 2008-2012 Barbie for Miss Barbell Productions
+  Copyright (C) 2008-2013 Barbie for Miss Barbell Productions
   All Rights Reserved.
 
   This module is free software; you can redistribute it and/or

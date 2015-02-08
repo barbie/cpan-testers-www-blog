6cc21479e293a919edc014cb3cc35042894b520b
Barbie <barbie@missbarbell.co.uk>
2015-02-08 19:32:30 +0000

    updated copyright year

--- lib/CPAN/Testers/WWW/Blog.pm
+++ lib/CPAN/Testers/WWW/Blog.pm
@@ -76,7 +76,7 @@ Miss Barbell Productions, L<http://www.missbarbell.co.uk/>
 
 =head1 COPYRIGHT & LICENSE
 
-  Copyright (C) 2008-2014 Barbie for Miss Barbell Productions
+  Copyright (C) 2008-2015 Barbie for Miss Barbell Productions
   All Rights Reserved.
 
   This module is free software; you can redistribute it and/or
